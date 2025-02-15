587dbe0728bf8de350ac210214f55d9f90f465fa
Agustín Gutiérrez Mora <118948002+agugutierrezz@users.noreply.github.com>
2025-02-15 00:58:40 -0300

    Update categoria.js

--- FrontEnd/tickets-react/src/utils/categoria.js
+++ FrontEnd/tickets-react/src/utils/categoria.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchCategorias = async () => {
     try {
-        const response = await axios.get('http://localhost:8000/api/categorias/');
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/categorias/`);
         return response.data;
     } catch (error) {
         console.error('Error al obtener las categorías:', error);
